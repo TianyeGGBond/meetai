58e40deb132066d1b33a46c9421bfb8014337b79
Sweetsweet <tianye35010@gmail.com>
2025-06-02 23:16:21 -0400

    07: dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
+import { DashboardNavbar } from '@/modules/dashboard/ui/components/dashboard-navbar';
 import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar';
 
 
@@ -6,12 +7,14 @@ type Props = {
     children: React.ReactNode;
 };
 
+
+
 function Layout({ children }: Props) {
     return (
         <SidebarProvider>
             <DashboardSidebar />
             <main className="bg-muted flex h-screen w-screen flex-col">
-
+                < DashboardNavbar />
                 {children}
             </main>
         </SidebarProvider>
