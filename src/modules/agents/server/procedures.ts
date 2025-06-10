c227a64de029af4079608c1c8ee56878d0e5f739
Sweetsweet <tianye35010@gmail.com>
2025-06-10 16:33:15 -0400

    14:agent page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,23 +5,28 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init"
 import { agentsInsertSchema } from "./schemas";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 
 // import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
-
-    //TODO: Change `getOne` to use `protectedProcedure`
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
-        .query(async ({ input }) => {
+        .query(async ({ input, ctx }) => {
             const [existingAgent] = await db
                 .select({
                     meetingCount: sql<number>`6`,
                     ...getTableColumns(agents),
                 })
                 .from(agents)
-                .where(eq(agents.id, input.id))
+                .where(
+                    and(eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id))
+                );
+            if (!existingAgent) {
+                throw new TRPCError({ code: 'NOT_FOUND', message: 'Agent not found' });
+            }
             return existingAgent;
         }),
     getMany: protectedProcedure
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
 
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
@@ -13,6 +15,7 @@ import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+    const router = useRouter();
     const [filters, setFilters] = useAgentsFilters();
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -21,7 +24,11 @@ export const AgentsView = () => {
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns} />
+            <DataTable
+                data={data.items}
+                columns={columns}
+                onRowClick={(row) => router.push(`/agents/${row.id}`)}
+            />
             <DataPagination
                 page={filters.page}
                 totalPages={data.totalPages}
