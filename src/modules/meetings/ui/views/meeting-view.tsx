98ec600532ec7a0877279917f442c6415880a860
Sweetsweet <tianye35010@gmail.com>
2025-06-11 00:45:33 -0400

    17:meetings form

--- src/modules/meetings/ui/views/meeting-view.tsx
+++ src/modules/meetings/ui/views/meeting-view.tsx
@@ -9,7 +9,11 @@ import React from "react";
 const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
-    return <div>{JSON.stringify(data?.items)}</div>;
+    return (
+        <div className="overflow-x-scroll">
+            {/* {JSON.stringify(data)} */}
+        </div>
+    );
 };
 
 export default MeetingsView;
